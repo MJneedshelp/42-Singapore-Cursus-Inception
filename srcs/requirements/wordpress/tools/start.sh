1487a669a9e5d5f798b04511abbed0d1e21b7f94
mintan <mintan@student.42singapore.sg>
2026-02-17 14:32:17 +0800

    Cleaned up env variables for wordpress and reordered activities in the start.sh for wordpress

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
+# -e: Exit on error. -u: undefined variables as errors | useful for debugging
+set -eu
+
 # Read the secrets
 DB_PASSWORD=$(cat /run/secrets/db_password)
-DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
-
-# Wait for MariaDB to be up
-echo "Maria are you up..."
-until mysqladmin ping -h"$DB_HOST" --silent; do
-	sleep 1
-done
+WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 
 cd /var/www/html
+
+# Download wordpress core if missing
+echo "Check if WordPress core is downloaded already"
+if [ ! -f wp-load.php ]; then
+	echo "WordPress core not found. Downloading WordPress core..."
+	wp core download --allow-root
+	echo "WordPress core downloaded"
+else
+	echo "WordPress core is already downloaded"
+fi
 
 # Create wp-config.php if missing
 if [ ! -f wp-config.php ]; then
@@ -23,12 +30,18 @@ if [ ! -f wp-config.php ]; then
 		--allow-root
 fi
 
+# Wait for MariaDB to be up
+until mysqladmin ping -h"$DB_HOST" --silent; do
+	echo "Maria are you up..."
+	sleep 1
+done
+
 # Install WordPress if not installed
 if ! wp core is-installed --allow-root; then
 	echo "Installing WordPress..."
 	wp core install \
 		--url="$DOMAIN_NAME" \
-		--title="Inception Site" \
+		--title="$WP_SITE_TITLE" \
 		--admin_user="$WP_ADMIN_USER" \
 		--admin_password="$WP_ADMIN_PASSWORD" \
 		--admin_email="$WP_ADMIN_EMAIL" \
